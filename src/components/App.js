e3bd06433fc968998fa67da5ee8a285072078678
Thomas Kerbrat <kerbrat@outlook.com>
2017-08-07 00:19:21 +0200

    Compute the average base stats per type of pokemons

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link, Redirect, } from 'react-router-dom';
+import { BrowserRouter, Route, Link, } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -75,7 +75,88 @@ class SearchablePokemonList extends Component {
   }
 
   componentDidMount() {
-    // TODO: Load some pokemons from the IndexedDB.
+    Promise.all([
+      fetch('/json/pokemon.json').then(response => response.json()),
+      fetch('/json/stats.json').then(response => response.json()),
+      fetch('/json/pokemon_stats.json').then(response => response.json()),
+      fetch('/json/types.json').then(response => response.json()),
+      fetch('/json/pokemon_types.json').then(response => response.json()),
+    ]).then(response => {
+      const [
+        pokemons, // id, identifier, species_id, height, weight, base_experience, order, is_default,
+        stats, // id, damage_class_id, identifier, is_battle_only, game_index,
+        pokemon_stats, // pokemon_id, stat_id, base_stat, effort,
+        types, // id, identifier, generation_id, damage_class_id,
+        pokemon_types, // pokemon_id, type_id, slot,
+      ] = response;
+
+      const pokemonsMap = new Map(pokemons.map(obj => [obj.id, obj]));
+      const statsMap = new Map(stats.map(obj => [obj.id, obj]));
+      const typesMap = new Map(types.map(obj => [obj.id, obj]));
+
+      const baseStatsByTypes = new Map(); // Map<type_id, Map<stat_id, { values: base_stat[], average: number }>>
+
+      // console.time('computations');
+
+      pokemon_types.forEach(record => {
+        const pokemon = pokemonsMap.get(record.pokemon_id);
+        const type = typesMap.get(record.type_id);
+
+        if (!Array.isArray(pokemon.types)) {
+          pokemon.types = [];
+        }
+
+        if (!baseStatsByTypes.has(record.type_id)) {
+          baseStatsByTypes.set(record.type_id, new Map());
+        }
+
+        pokemon.types.push({
+          type: type,
+          slot: record.slot,
+        });
+      });
+
+      pokemon_stats.forEach(record => {
+        const pokemon = pokemonsMap.get(record.pokemon_id);
+        const stat = statsMap.get(record.stat_id);
+
+        if (!Array.isArray(pokemon.stats)) {
+          pokemon.stats = [];
+        }
+
+        pokemon.types.forEach(type => {
+          const typeMap = baseStatsByTypes.get(type.type.id);
+
+          if (!typeMap.has(record.stat_id)) {
+            typeMap.set(record.stat_id, { values: [], average: -1 });
+          }
+
+          typeMap.get(record.stat_id).values.push(record.base_stat);
+        });
+
+        pokemon.stats.push({
+          stat: stat,
+          base_stat: record.base_stat,
+          effort: record.effort,
+        });
+      });
+
+      baseStatsByTypes.forEach((baseStatsMap, typeKey) => {
+        baseStatsMap.forEach((baseStats, statKey) => {
+          const values = baseStats.values;
+          baseStats.average = values.reduce((sum, stat) => sum + stat, 0) / values.length;
+          delete baseStats.values;
+        });
+      });
+
+      // console.timeEnd('computations');
+
+      window.___pokemons = pokemons;
+
+      console.log(pokemons);
+      console.log(baseStatsByTypes);
+    })
+      .catch(error => console.error(error))
   }
 
   handleSearchTextInput(searchText) {
